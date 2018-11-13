fcca66a8a1e321e795403b9440c566e4b0673895
Kiley Bobbitt <jkileybob@aol.com>
2018-11-13 20:06:41 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [Lindsay, Kiley, Ted, Giuseppe]
+  ["Lindsay", "Kiley", "Ted", "Giuseppe"]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
